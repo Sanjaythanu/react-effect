42cb3973b1376b890dd844ce8a5293c4e32e8355
Sanjay Sri Thanu Malaiyaan <sanjaychittu2005@gmail.com>
2025-12-30 09:27:12 +0530

    wig

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ function App() {
       <h1>My news Application</h1>
       {/* Buttons to change the category */}
       <button onClick={()=> setCategory("general")}>General</button>
-      <button onClick={()=> setCategory("technology")}>Technology</button>
-      {/* Passing the category to News */}
       <News category={category}/>
     </div>
   );
